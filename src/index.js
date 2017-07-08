5221ba95ac367306c0658e0175ea69b2f52409f8
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-08 09:37:55 +0100

    Add some test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-const insArr = () => 1 + 1;
+const insArr = (arr, item, index) => {
+
+    if (!arr) {
+        return;
+    }
+
+    if(!Array.isArray(arr)) {
+        return arr;
+    }
+
+    if(!item) {
+        return arr;
+    }
+    
+    arr.push(item);
+    return arr;
+}
+
 export default insArr;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,21 @@
 import insArr from './';
 
-test('Insert an item into array', () => {
-    expect(insArr()).toBe(2);
+describe('Sum', () => {
+    const arr = [];
+    
+    test('Should return undefined if no array is passed', () => {
+        expect(insArr()).toEqual(undefined);
+    });
+
+    test('Should return passed value if value is not array', () => {
+        expect(insArr(10)).toEqual(10);
+    });
+
+    test('Should return passed array if value is not passed', () => {
+        expect(insArr(arr)).toEqual(arr);
+    });
+
+    test('Item should be added into an array', () => {
+        expect(insArr(arr, 10)).toEqual([10]);        
+    });
 });
